90a008cda1185b96950816d86bba9f0919a81a90
andremoss800 <andremoss800@gmail.com>
2025-08-05 21:19:20 +0300

    edited the api for details

--- lib/api/poke_api_service.dart
+++ lib/api/poke_api_service.dart
@@ -51,4 +51,59 @@ class PokeApiService {
       throw Exception('Failed to fetch Pokémon details for $name');
     }
   }
+
+  // detailed pokemon call
+  Future<PokemonDetail> getPokemonDetail(String name) async {
+    // 1. Basic Pokémon info
+    final response = await http.get(Uri.parse('$baseUrl/pokemon/$name'));
+    if (response.statusCode != 200) throw Exception('Failed to load Pokémon');
+
+    final data = json.decode(response.body);
+
+    final imageUrl =
+        data['sprites']['other']['official-artwork']['front_default'] ?? '';
+    final type = data['types'][0]['type']['name'];
+
+    final stats = data['stats'] as List;
+
+    int getStat(String statName) {
+      return stats.firstWhere(
+            (s) => s['stat']['name'] == statName,
+            orElse: () => {'base_stat': 0},
+          )['base_stat'] ??
+          0;
+    }
+
+    final hp = getStat('hp');
+    final attack = getStat('attack');
+    final defense = getStat('defense');
+
+    // 2. Description (from species)
+    final speciesRes = await http.get(
+      Uri.parse('$baseUrl/pokemon-species/$name'),
+    );
+    if (speciesRes.statusCode != 200) throw Exception('Failed to load species');
+
+    final speciesData = json.decode(speciesRes.body);
+
+    final flavorTextEntry = (speciesData['flavor_text_entries'] as List)
+        .firstWhere(
+          (entry) => entry['language']['name'] == 'en',
+          orElse: () => {'flavor_text': 'No description available.'},
+        );
+
+    final description = (flavorTextEntry['flavor_text'] as String)
+        .replaceAll('\n', ' ')
+        .replaceAll('\f', ' ');
+
+    return PokemonDetail(
+      name: name,
+      imageUrl: imageUrl,
+      type: type,
+      hp: hp,
+      attack: attack,
+      defense: defense,
+      description: description,
+    );
+  }
 }
--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -4,3 +4,23 @@ class PokemonBasic {
 
   PokemonBasic({required this.name, required this.imageUrl});
 }
+
+class PokemonDetail {
+  final String name;
+  final String imageUrl;
+  final String type;
+  final int hp;
+  final int attack;
+  final int defense;
+  final String description;
+
+  PokemonDetail({
+    required this.name,
+    required this.imageUrl,
+    required this.type,
+    required this.hp,
+    required this.attack,
+    required this.defense,
+    required this.description,
+  });
+}
